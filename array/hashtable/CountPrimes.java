756638dd7d4cc026f44f3de730e53de0e95720f1
Rui Tian <nju081190063@gmail.com>
2015-08-06 22:19:11 -0700

    Count Primes #204

---
+++ array/hashtable/CountPrimes.java
@@ -0,0 +1,41 @@
+package hashtable;
+
+public class CountPrimes {
+
+	public static int countPrimes(int n) {
+		int res = 0;
+		boolean[] pri = new boolean[n + 1];
+		for (int i = 2; i < Math.sqrt(n); i++) {
+			if (isPrime(i)) {
+				int k = 2;
+				while (k * i < n) {
+					pri[k * i] = true;
+					k++;
+				}
+			}
+		}
+		for (int i = 2; i < pri.length; i++) {
+			System.out.println(pri[i]);
+		}
+		for (int i = 2; i < n; i++) {
+			res += pri[i] ? 0 : 1;
+		}
+		return res;
+	}
+
+	public static boolean isPrime(int n) {
+		if (n < 2) {
+			return false;
+		}
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(countPrimes(5));
+	}
+}
