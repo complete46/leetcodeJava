2a77907e1259eb6488b9e10dfa0d68f031f59daa
Rui Tian <nju081190063@gmail.com>
2015-08-06 22:35:43 -0700

    Anagrams #49

---
+++ array/hashtable/Anagrams.java
@@ -0,0 +1,32 @@
+package hashtable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Anagrams {
+	public List<String> anagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        List<String> res = new ArrayList<String>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] s = strs[i].toCharArray();
+            Arrays.sort(s);
+            String newStr = new String(s);
+            
+            if (!map.containsKey(newStr)) {
+                map.put(newStr, new ArrayList<String>());
+            }
+            map.get(newStr).add(strs[i]);
+        }
+        
+        for (List<String> list : map.values()) {
+            if (list.size() > 1) {
+                for (String str : list) {
+                    res.add(str);
+                }
+            }
+        }
+        
+        return res;
+    }
+}
