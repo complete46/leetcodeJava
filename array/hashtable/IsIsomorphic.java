f840a779c1ac22d027fc7d038d56e75ef7e8585a
Rui Tian <nju081190063@gmail.com>
2015-08-08 01:59:31 -0700

    Isomorphic String #205

---
+++ array/hashtable/IsIsomorphic.java
@@ -0,0 +1,23 @@
+package hashtable;
+
+public class IsIsomorphic {
+	public static boolean isIsomorphic(String s, String t) {
+		int[] maps = new int[256];
+        int[] mapt = new int[256];
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (maps[s.charAt(i)] != mapt[t.charAt(i)]) {
+                return false;
+            }
+            
+            maps[s.charAt(i)] = i + 11;
+            mapt[t.charAt(i)] = i + 11;
+        }
+        
+        return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isIsomorphic("aba", "baa"));
+	}
+}
