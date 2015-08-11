86cc550436af954dcbb4c4bc2a436e600c092837
Rui Tian <nju081190063@gmail.com>
2015-08-10 23:45:34 -0700

    Reverse Nodes in k-Group  #25

---
+++ array/hashtable/ReverseKGroup.java
@@ -0,0 +1,41 @@
+package hashtable;
+
+public class ReverseKGroup {
+	public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) {
+            return head;
+        }
+        
+        int i = 0;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (head != null) {
+            i++;
+            if (i % k == 0) {
+                prev = reverse(prev, head.next);
+                head = prev.next;
+            } else {
+                head = head.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+    
+    public ListNode reverse(ListNode prev, ListNode next) {
+        ListNode last = prev.next;
+        ListNode cur = last.next;
+        
+        while (cur != next) {
+            last.next = cur.next;
+            cur.next = prev.next;
+            prev.next = cur;
+            cur = last.next;
+        }
+        
+        return last;
+    }
+}
