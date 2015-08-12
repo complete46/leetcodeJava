e9823d3857e39e8301fff2381b6b2d5f31082539
Rui Tian <nju081190063@gmail.com>
2015-08-11 23:02:46 -0700

    Partition List #86

---
+++ array/hashtable/Partition.java
@@ -0,0 +1,31 @@
+package hashtable;
+
+public class Partition {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (fast.next != null) {
+            if (fast.next.val < x) {
+                if (fast != slow) {
+                    ListNode temp = slow.next;
+                    slow.next = fast.next;
+                    fast.next = fast.next.next;
+                    slow.next.next = temp;
+                } else {
+                    fast = fast.next;
+                }
+                slow = slow.next;
+            } else {
+                fast = fast.next;
+            }
+        }
+        return dummy.next;
+    }
+}
