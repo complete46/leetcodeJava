b94f71e4533ad381419be4a1521a9f98bc70c3d8
Rui Tian <nju081190063@gmail.com>
2015-08-13 00:56:06 -0700

    Binary Tree Level Order Traversal II #107

---
+++ array/tree/LevelOrderII.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class LevelOrderII {
+public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        level(res, root, 0);
+        
+        return res;
+    }
+    
+    public void level(List<List<Integer>> res, TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+        
+        ArrayList<Integer> item;
+        if (level == res.size()) {
+            item = new ArrayList<Integer>();
+            res.add(0, item);
+        } else {
+            item = (ArrayList<Integer>)res.get(res.size() - 1 - level);
+        }
+        
+        item.add(0, root.val);
+        
+        level(res, root.right, level + 1);
+        level(res, root.left, level + 1);
+        
+        return;
+    }
+}
