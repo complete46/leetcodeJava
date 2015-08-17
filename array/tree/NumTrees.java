a20f50e3aea1af390d6b40c73c5a189920ea00fb
Rui Tian <nju081190063@gmail.com>
2015-08-16 17:09:10 -0700

    Unique Binary Search Trees #95

---
+++ array/tree/NumTrees.java
@@ -0,0 +1,24 @@
+package tree;
+
+public class NumTrees {
+	public static int numTrees(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += (dp[j] * dp[i - j - 1]);
+            }
+        }
+        
+        return dp[n];
+    }
+	
+	public static void main(String[] args) {
+		int n = 2;
+		System.out.println(numTrees(n));
+	}
+}
