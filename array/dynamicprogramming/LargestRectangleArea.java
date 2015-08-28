243186fba1038de4ceff6807ffae4a1e4e949f91
Rui Tian <nju081190063@gmail.com>
2015-08-28 02:03:57 -0700

    Largest Rectangle in Histogram #84

---
+++ array/dynamicprogramming/LargestRectangleArea.java
@@ -0,0 +1,20 @@
+package dynamicprogramming;
+
+public class LargestRectangleArea {
+	public int largestRectangleArea(int[] height) {
+        Stack<Integer> s = new Stack<Integer>();
+        int max = 0;
+        for (int i = 0; i <= height.length; i++) {
+            int h = (i == height.length ? 0 : height[i]);
+            if (s.isEmpty() || height[s.peek()] <= h) {
+                s.push(i);
+            } else {
+                int top = s.pop();
+                max = Math.max(max, height[top] * (s.isEmpty() ? i : i - 1 - s.peek()));
+                i--;
+            }
+        }
+        
+        return max;
+    }
+}
