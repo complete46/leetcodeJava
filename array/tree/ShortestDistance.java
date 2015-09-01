a29d204e10c8d5b501568125106f382616b64c2c
Rui Tian <nju081190063@gmail.com>
2015-09-01 01:27:10 -0700

    Shortest Distance #243

---
+++ array/tree/ShortestDistance.java
@@ -0,0 +1,19 @@
+package tree;
+
+public class ShortestDistance {
+	public int shortestDistance(String[] words, String word1, String word2) {
+        int i = -1;
+        int j = -1;
+        int min = Integer.MAX_VALUE;
+        for (int k = 0; k < words.length; k++) {
+            if (words[k].equals(word1)) i = k;
+            if (words[k].equals(word2)) j = k;
+            
+            if (i != -1 && j != -1 && Math.abs(i - j) < min) {
+                min = Math.abs(i - j);
+            }
+        }
+        
+        return min;
+    }
+}
